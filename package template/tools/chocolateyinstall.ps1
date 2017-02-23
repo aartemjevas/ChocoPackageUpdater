fe3c0b35fc5785f2e04f93c592c100a2a32e866e
Aleksandras Artemjevas <aleksandras.artemjevas@runwaybpo.com>
2017-02-23 21:53:46 +0200

    initial

---
+++ package template/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'
+try {
+    $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    . $toolsDir\helpers.ps1
+    $Package = Get-Content "$toolsDir\Package.json" | Out-String | ConvertFrom-Json 
+    $url = Get-URL -Arch 32
+    $url64 = Get-URL -Arch 64
+
+    $packageArgs = @{
+      packageName   = $Package.Packagename
+      fileType      = $Package.Filetype
+      url           = $url
+      url64bit      = $url64
+      softwareName  = $Package.softwareName
+      checksum      = $Package.Checksum32
+      checksumType  = 'md5' 
+      checksum64    = $Package.Checksum64
+      checksumType64= 'md5' 
+      silentArgs    = ""
+      validExitCodes= @(0, 3010, 1641)
+    }
+
+    Install-ChocolateyPackage @packageArgs
+
+} 
+
+catch {
+    throw $_.expression.message
+}
+
